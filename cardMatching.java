b8eff0f974084a366c39da8fe8e888bf723734fd
Wilson Gao <65750807+GaoWilson81@users.noreply.github.com>
2022-09-24 17:27:11 -0400

    Update cardMatching.java

--- cardMatching.java
+++ cardMatching.java
@@ -7,27 +7,44 @@ public class cardMatching {
     //Since there will be rows and columns, 2D array will be used
     //It could be an int or String type since the cards can contain either one
 
-    protected static int row;
-    protected static int column;
+    protected static String difficulty;
+    protected static int row = 0;
+    protected static int column = 0;
     public static String[][] board = new String[9][9]; //creates a board with maximum of 9x9
     public static String[][] card = new String[9][9];
 
     public static void printWelcome() {
+        boolean valid = true;
         Scanner input = new Scanner(System.in); //This will be used for user input's row and columns for the board
         System.out.println("Welcome to cardGame.");
         System.out.println("====================");
-        System.out.println("Please enter the number of rows (1-9) you would like: ");
-        row = input.nextInt();
-        input.nextLine();
-        System.out.println("Please enter the number of columns (1-9) you would like: ");
-        column = input.nextInt();
-        input.nextLine();
-        System.out.println("Loading...");
-        if (row <= 0 || column <= 0) {
-            System.out.println("Invalid, please try again...");
+        while (valid) {
+            System.out.print("Please enter the difficulty you would like (easy, medium hard): ");
+            difficulty = input.nextLine().toLowerCase();
+            System.out.println(difficulty);
+            if (!difficulty.equals("easy") && !difficulty.equals("medium") && !difficulty.equals("hard")) {
+                System.out.println("Invalid, please try again...");
+                continue;
+            }
+        break;
+        }
+    }
+    public static void difficulty() {
+        if (difficulty.equals("easy")) {
+            row = 4;
+            column = 4;
+        }
+        if (difficulty.equals("medium")) {
+            row = 6;
+            column = 6;
+        }
+        if (difficulty.equals("hard")) {
+            row = 8;
+            column = 8;
         }
     }
     public static void showBoard() {
+        difficulty();
         for(int i = 0; i < row; i++) {
             if (row > 9) {
                 System.out.println("Please make sure the number of rows doesn't exceed 9"); //breaks if row exceeds 9
@@ -46,116 +63,41 @@ public class cardMatching {
         }
     }
     public static void randomCards(){
+        difficulty();
+        System.out.println(row);
+        char[] lowerCaseLetters = "abcdefghijklmnopqrstuvuwyz".toCharArray();
+        char[] upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
         Random random = new Random(); //random default is 0-1
         int shuffle;
-        ArrayList<String> newCard = new ArrayList<>(); //used String here since it'll provide more varieties for the cards
-        newCard.add("a");
-        newCard.add("b");
-        newCard.add("c");
-        newCard.add("d");
-        newCard.add("e");
-        newCard.add("f");
-        newCard.add("g");
-        newCard.add("h");
-	newCard.add("i");
-        newCard.add("j");
-        newCard.add("k");
-        newCard.add("l");
-	newCard.add("m");
-        newCard.add("n");
-        newCard.add("o");
-        newCard.add("p");
-	newCard.add("q");
-	newCard.add("r");
-        newCard.add("s");
-        newCard.add("t");
-        newCard.add("u");
-        newCard.add("v");
-        newCard.add("w");
-        newCard.add("x");
-        newCard.add("y");
-        newCard.add("z");
-
-        newCard.add("a");
-        newCard.add("b");
-        newCard.add("c");
-        newCard.add("d");
-        newCard.add("e");
-        newCard.add("f");
-        newCard.add("g");
-        newCard.add("h");
-	newCard.add("i");
-        newCard.add("j");
-        newCard.add("k");
-        newCard.add("l");
-	newCard.add("m");
-        newCard.add("n");
-        newCard.add("o");
-        newCard.add("p");
-	newCard.add("q");
-	newCard.add("r");
-        newCard.add("s");
-        newCard.add("t");
-        newCard.add("u");
-        newCard.add("v");
-        newCard.add("w");
-        newCard.add("x");
-        newCard.add("y");
-        newCard.add("z");
-
-        newCard.add("A");
-        newCard.add("B");
-        newCard.add("C");
-        newCard.add("D");
-        newCard.add("E");
-        newCard.add("F");
-        newCard.add("G");
-        newCard.add("H");
-	newCard.add("I");
-        newCard.add("J");
-        newCard.add("K");
-        newCard.add("L");
-	newCard.add("M");
-        newCard.add("N");
-        newCard.add("O");
-        newCard.add("P");
-	newCard.add("Q");
-	newCard.add("R");
-        newCard.add("S");
-        newCard.add("T");
-        newCard.add("U");
-        newCard.add("V");
-        newCard.add("W");
-        newCard.add("X");
-        newCard.add("Y");
-        newCard.add("Z");
-
-        newCard.add("A");
-        newCard.add("B");
-        newCard.add("C");
-        newCard.add("D");
-        newCard.add("E");
-        newCard.add("F");
-        newCard.add("G");
-        newCard.add("H");
-	newCard.add("I");
-        newCard.add("J");
-        newCard.add("K");
-        newCard.add("L");
-	newCard.add("M");
-        newCard.add("N");
-        newCard.add("O");
-        newCard.add("P");
-	newCard.add("Q");
-	newCard.add("R");
-        newCard.add("S");
-        newCard.add("T");
-        newCard.add("U");
-        newCard.add("V");
-        newCard.add("W");
-        newCard.add("X");
-        newCard.add("Y");
-        newCard.add("Z");
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        ArrayList<String> newCard = new ArrayList<>(); 
+        if (difficulty.equals("easy")) {
+            while (a < 2) {
+                for (int i = 0; i < row * column / 2; i++) {
+                    newCard.add(lowerCaseLetters[i] + "");
+                }
+                a++;
+            }
+        }
+        if (difficulty.equals("medium")) {
+            while (b < 2) {
+                for (int i = 0; i < row * column / 2; i++) {
+                    newCard.add(lowerCaseLetters[i] + "");
+                }
+                b++;
+            }
+        }
+        if (difficulty.equals("hard")) {
+            while (c < 2) {
+                for (int i = 0; i < row * column / 2 / 2; i++) {
+                    newCard.add(lowerCaseLetters[i] + "");
+                    newCard.add(upperCaseLetters[i] + "");
+                }
+                c++;
+            }
+        }
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 shuffle = random.nextInt(newCard.size()); //randomizes card
@@ -166,6 +108,7 @@ public class cardMatching {
         }
     }
     public static boolean gameOver() {
+        difficulty();
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 if (board[i][j].equals(" - ")) {
@@ -176,23 +119,22 @@ public class cardMatching {
         return true;
     }
     public static void match(String[][] newCard) { //takes in card to set board to the value of card
+        difficulty();
         Scanner input = new Scanner(System.in);
         boolean looping = true;
         while(looping) {
             if (!gameOver()) {
-                System.out.println("Which row? (1-9)");
+                System.out.println("Which row?");
                 int newRow = input.nextInt();
-                input.nextLine();
-                System.out.println("Which column? (1-9)");
+                System.out.println("Which column?");
                 int newColumn = input.nextInt();
-                input.nextLine();
 
                 if (newRow <= 0 || newColumn <= 0) {
                     System.out.println("Make sure the row/column is at least one or greater");
-                    break;
-                } else if (newRow > 9 || newColumn > 9) {
-                    System.out.println("Make sure the row/column is lesser than/equal to 9");
-                    break;
+                    continue;
+                } else if (newRow > row || newColumn > column) {
+                    System.out.printf("Make sure the row/column is lesser than/equal to %d\n", row);
+                    continue;
                 }
                 //if not equal to " - ", that means the user already entered something
                 if (!board[newRow - 1][newColumn - 1].equals(" - ")) {
@@ -204,12 +146,18 @@ public class cardMatching {
                     showBoard();
                 }
                 //repeats which row/column input because you need two input to match
-                System.out.println("Once again, I shall ask, which row? (1-9)");
+                System.out.println("Once again, I shall ask, which row? ");
                 int newRow2 = input.nextInt();
-                input.nextLine();
-                System.out.println("And...which column? (1-9)");
+                System.out.println("And...which column? ");
                 int newColumn2 = input.nextInt();
-                input.nextLine();
+
+                if (newRow2 <= 0 || newColumn2 <= 0) {
+                    System.out.println("Make sure the row/column is at least one or greater");
+                    continue;
+                } else if (newRow2 > row || newColumn2 > column) {
+                    System.out.printf("Make sure the row/column is lesser than/equal to %d\n", row);
+                    continue;
+                }
                 if (!board[newRow2 - 1][newColumn2 - 1].equals(" - ")) {
                     System.out.println("This place is taken...try somewhere different");
                     board[newRow - 1][newColumn - 1] = " - "; //sets first input to " - " because second input is already entered but the first was not
@@ -223,14 +171,14 @@ public class cardMatching {
                         System.out.println("Not bad, you got it right");
                     } else {
                         showBoard();
-                        System.out.println("Incorrect...noob");
+                        System.out.println("Incorrect...");
                         board[newRow - 1][newColumn - 1] = " - ";
                         board[newRow2 - 1][newColumn2 - 1] = " - ";
                         showBoard();
                     }
                 }
-            } else {
-                System.out.println(".-.-.Game Over.-.-.");
+            } else if (row != 0 && column != 0) {
+                System.out.println(".-.-.GGEZ.-.-.");
                 break;
             }
         }
@@ -247,7 +195,7 @@ public class cardMatching {
                 System.out.println("Leaving...");
                 break;
             } else if (create.equals("new")) {
-				printWelcome();
+                printWelcome();
                 randomCards();
                 for (int i = 0; i < row; i++) {
                     for (int j = 0; j < column; j++) {
@@ -266,6 +214,6 @@ public class cardMatching {
 
     public static void main(String[] args) {
 
-		createGame();
-	}
+        createGame();
+    }
 }
